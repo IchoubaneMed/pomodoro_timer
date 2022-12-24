66b311d39dc8f100aa16d5539142daea5ff49f57
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-24 12:37:17 +0100

    Write the body function of the Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,16 @@
-function Timer() {
+function Timer({label, minutes, seconds, play, reset}) {
     return(
         <div>
-            Timer
+            <h3 className="label">{label}</h3>
+            <h3 className="time-left">
+                {
+                    (minutes < 0 ? "0" + minutes : minutes)
+                    + ":" +
+                    (seconds < 10 ? "0" + seconds : seconds)
+                }
+            </h3>
+            <button onClick={play}>Play/Stop</button>
+            <button onClick={reset}>Reset</button>
         </div>
     );
 }
