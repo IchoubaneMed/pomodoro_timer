fd2836902b4b426d3c1bd7b48ed5fdfae6054e56
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-24 12:15:36 +0100

    Write the body function of the Break component

--- src/components/Break.js
+++ src/components/Break.js
@@ -1,7 +1,10 @@
-function Break() {
+function Break({changeTime, breakTime}) {
     return(
         <div>
-            Break
+            <h3 className="label">Break</h3>
+            <button className="btn" onClick={() => changeTime(-1, "break")}>decrease</button>
+            <h3>{breakTime}</h3>
+            <button className="btn" onClick={() => changeTime(1, "break")}>Increase</button>
         </div>
     );
 }
