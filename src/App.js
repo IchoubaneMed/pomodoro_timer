859a9357453a13d3786ee1413e347898222da47e
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-12-24 12:56:27 +0100

    Added useEffect hooks to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import Break from './components/Break';
 import Session from './components/Session';
@@ -16,6 +16,68 @@ function App() {
   const [onSession, setOnSession] = useState(true);
   const [onBreak, setOnBreak] = useState(false);
   const [label, setLabel] = useState("Session");
+
+  // to update the minutes and seconds with the sessionTime value
+  useEffect(() => {
+    if(onSession) {
+      setMinutes(sessionTime * 60 / 60);
+      setSeconds(sessionTime * 60 % 60); 
+    }
+  }, [onSession, sessionTime])
+
+  // to update the minutes and seconds with the breakTime value
+  useEffect(() => {
+    if(onBreak) {
+      setMinutes(breakTime * 60 / 60);
+      setSeconds(breakTime * 60 % 60); 
+    }
+  }, [onBreak, breakTime]);
+
+  // to switch from the session counter to the break counter
+  useEffect(() => {
+    if(start && onSession && minutes === 0 && seconds === 0) {
+      //playSound();
+      setMinutes(breakTime * 60 / 60);
+      setSeconds(breakTime * 60 % 60);
+      setOnSession(false);
+      setOnBreak(true);
+      setLabel("Break");
+    }
+  }, [start, onSession, minutes, seconds, breakTime])
+
+  // to switch from the break counter to the session counter
+  useEffect(() => {
+    if(start && onBreak && minutes === 0 && seconds === 0) {
+      //playSound();
+      setMinutes(sessionTime * 60 / 60);
+      setSeconds(sessionTime * 60 % 60); 
+      setOnSession(true);
+      setOnBreak(false);
+      setLabel("Session")
+    }
+  }, [start, onBreak, minutes, seconds, sessionTime])
+
+  // to decrese the timer if the session is on
+  useEffect(() => {
+    if(start && onSession && minutes > 0 && seconds === 0) {
+        const interval = setInterval(() => {
+              setSeconds(59);
+              setMinutes(pre => pre - 1);
+        }, 1000)
+        return () => clearInterval(interval);
+      }
+    
+  }, [start, minutes, seconds, onSession]);
+  
+  useEffect(() => {
+    if(start && onSession && seconds > 0){
+        const interval = setInterval(() => {
+              setSeconds(pre => pre - 1);
+        }, 1000)
+        return () => clearInterval(interval);
+      }
+    
+  }, [start, onSession, minutes, seconds])
 
   // to prevent the session or break length from being <= 0 or > 60 
   const changeTime = (amount, type) => {
@@ -35,6 +97,27 @@ function App() {
       return;
     }
   }
+
+
+  // to decrease the timer if the break is on
+  useEffect(() => {
+    if(start && onBreak && minutes > 0 && seconds === 0) {
+        const interval = setInterval(() => {
+              setSeconds(59);
+              setMinutes(pre => pre - 1);
+        }, 1000)
+        return () => clearInterval(interval);
+      }
+  }, [start, minutes, seconds, onBreak])
+  
+  useEffect(() => {
+    if(start && onBreak && seconds > 0){
+        const interval = setInterval(() => {
+              setSeconds(pre => pre - 1);
+        }, 1000)
+        return () => clearInterval(interval);
+      }
+  }, [start, onBreak, minutes, seconds])
 
   // to enable and disable the start variable
   const play = () => {
